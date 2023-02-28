e82c1cdfbd58e49173d64b5bf083f0ef3a8b7237
Rilton Franzone <riltonfranzone@gmail.com>
2023-02-28 10:31:20 -0300

    color converter

--- converter.test.ts
+++ converter.test.ts
@@ -5,14 +5,14 @@ import { converter } from "./converter";
 
 const tests = [
   { initial: "simple.css", expected: "simple_expected.css" },
-  // { initial: "advanced.css", expected: "advanced.css" },
+  { initial: "advanced.css", expected: "advanced_expected.css" },
 ];
 
 tests.forEach(({ initial, expected }) => {
   it(`converts ${initial} file into ${expected}`, () => {
     const expectedResult = fs.readFileSync(`colors/${expected}`, "utf-8");
     const actualResult = converter(`colors/${initial}`);
-    assert.equal(actualResult, expectedResult)
+    assert.deepEqual(actualResult, expectedResult)
   })
 })
 
--- converter.ts
+++ converter.ts
@@ -1,23 +1,23 @@
 import fs from 'fs';
 
-const converter = (filename: string) => {
+const converter = (filename: string): string => {
   let cssFile = fs.readFileSync(filename, 'utf-8');
-  const colors = cssFile.match(/#.*?;/g);
+  const colors = cssFile.match(/#.*?;/g); // find all colors
 
-  colors?.forEach((colorString) => {
-    const color = colorString.replace("#", '').replace(';', '');
+  colors?.forEach((hexString) => {
+    let color = hexString.replace("#", '').replace(';', '');
+    const digits = color.length;
+    if(digits === 3 || digits === 4) color = color.split("").map(item => item.repeat(2)).join("");
     let hexadecimal = parseInt(color, 16);
-
     const output: number[] = [];
-
-    while(hexadecimal > 0) {
-      const colorNum = hexadecimal & 0xff // take only two bytes
-      hexadecimal = hexadecimal >> 8; // move two bytes right
-      output.push(colorNum)
+    for(let i = 0; i < color.length / 2; i++) {
+      const colorNum = hexadecimal & 0xff // take only one byte
+      hexadecimal = hexadecimal >> 8; // move one byte to the right
+      output.push(colorNum);
     }
-
-    const [blue, green, red] = output;
-    cssFile = cssFile.replace(colorString, `rgb(${red} ${green} ${blue});`)
+    const [red = 0, green = 0, blue = 0, alpha] = output.reverse();
+    const rgbString = alpha !== undefined ? `rgba(${red} ${green} ${blue} ${alpha / 256});` : `rgb(${red} ${green} ${blue});`;
+    cssFile = cssFile.replace(hexString, rgbString)
   })
 
   return cssFile;
